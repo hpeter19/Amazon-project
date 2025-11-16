e23d753c1f7bc90b69ce6eff7c0813cb922cd5b4
hpeter19 <njoshpitar@gmail.com>
2025-11-16 20:39:35 +0300

    adding objects

---
+++ scripts/amazon.js
@@ -0,0 +1,26 @@
+const products=[{
+    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:'Black and Gray',
+    rating:{
+        stars:4.5,
+        count:87
+    },
+    priceCents:10.90
+
+},{
+    image:'images/products/intermediate-composite-basketball.jpg',
+    name:'Intermediate Size Basketball',
+    rating:{
+        stars:4,
+        count:125
+    },
+    priceCents:2095
+},{
+     image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:' Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        stars:4.5,
+        count:80
+    },
+    priceCents:799
+}];
