582d394db37475c5f9a52b6d421074998e25d1ca
hpeter19 <njoshpitar@gmail.com>
2025-11-19 14:31:02 +0300

    external library

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {cart,removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+hello()
 
 let catSummaryHTML='';
 
