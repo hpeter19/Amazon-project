4762b4e4db143cf8fd146e5c7f65abd795c180a1
hpeter19 <njoshpitar@gmail.com>
2025-11-19 21:39:49 +0300

    delivery date for todays date

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,8 @@ import {cart,removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'; 
+import {deliveryOptions} from '../data/deliveryOptions.js';
 
 hello()
 const today=dayjs();
@@ -59,56 +60,108 @@ cart.forEach((cartItem)=>{
                 </div>
             </div>
 
-            <div class="delivery-options">
-                <div class="delivery-options-title">
-                Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                <div>
-                    <div class="delivery-option-date">
-                    Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                    FREE Shipping
-                    </div>
-                </div>
-                </div>
-                <div class="delivery-option">
-                <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                <div>
-                    <div class="delivery-option-date">
-                    Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                    $4.99 - Shipping
-                    </div>
-                </div>
-                </div>
-                <div class="delivery-option">
-                <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                <div>
-                    <div class="delivery-option-date">
-                    Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                    $9.99 - Shipping
+                    <div class="delivery-options">
+                        <div class="delivery-options-title">
+                        Choose a delivery option:
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio" checked
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Tuesday, June 21
+                            </div>
+                            <div class="delivery-option-price">
+                            FREE Shipping
+                            </div>
+                        </div>
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio"
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Wednesday, June 15
+                            </div>
+                            <div class="delivery-option-price">
+                            $4.99 - Shipping
+                            </div>
+                        </div>
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio"
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Monday, June 13
+                            </div>
+                            <div class="delivery-option-price">
+                            $9.99 - Shipping
+                            </div>
+                        </div>
                     </div>
                 </div>
                 </div>
             </div>
-            </div>
-        </div>
-    
-    
-    `;
-});
+        
+        
+        `;
+    });
+
+    function deliveryOptionsHTML(){
+        deliveryOptions.forEach((deliveryOption)=>{
+            const today=dayjs();
+
+        ` 
+        <div class="delivery-options">
+                        <div class="delivery-options-title">
+                        Choose a delivery option:
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio" checked
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Tuesday, June 21
+                            </div>
+                            <div class="delivery-option-price">
+                            FREE Shipping
+                            </div>
+                        </div>
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio"
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Wednesday, June 15
+                            </div>
+                            <div class="delivery-option-price">
+                            $4.99 - Shipping
+                            </div>
+                        </div>
+                        </div>
+                        <div class="delivery-option">
+                        <input type="radio"
+                            class="delivery-option-input"
+                            name="delivery-option-${matchingProduct.id}">
+                        <div>
+                            <div class="delivery-option-date">
+                            Monday, June 13
+                            </div>
+                            <div class="delivery-option-price">
+                            $9.99 - Shipping
+                            </div>
+                        </div>
+                    </div>
+                    `;
+    });
+}
 
 document.querySelector('.js-order-summary').innerHTML=catSummaryHTML;
 
