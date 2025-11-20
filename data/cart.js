57bcf65c34c5eea6dacaf4972c175a74db3863cb
hpeter19 <njoshpitar@gmail.com>
2025-11-20 11:13:11 +0300

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -20,7 +20,6 @@ function saveToStorage(){
 export function addToCart(productId){
      
       let matchingItem;
-
       cart.forEach((cartItem)=>{
           if(productId===cartItem.productId){
             matchingItem=cartItem;
@@ -50,4 +49,17 @@ export function removeFromCart(productId){
    cart=newCart;
 
    saveToStorage();
+}
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+     let matchingItem;
+      cart.forEach((cartItem)=>{
+          if(productId===cartItem.productId){
+            matchingItem=cartItem;
+          }
+      });
+
+    matchingItem.deliveryOptionId =deliveryOptionId;
+    
+    saveToStorage();
 }
