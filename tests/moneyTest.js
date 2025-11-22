f7378f3a0ae007bc3251d774382bd29019db9e87
hpeter19 <njoshpitar@gmail.com>
2025-11-22 22:51:26 +0300

    add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -5,3 +5,15 @@ if(formatCurrency(2095)==='20.95'){
 }else{
    console.log("failed") ;
 }
+
+if(formatCurrency(0)==='0.00'){
+        console.log('passed');
+}else{
+   console.log("failed") ; 
+}
+
+if(formatCurrency(2000.5)==='20.01'){
+        console.log('passed');
+}else{
+   console.log("failed") ; 
+}
